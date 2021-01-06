521a130c29f807dd50465cca513b7ba609f1399c
dylan-apera <74391707+dylan-apera@users.noreply.github.com>
2021-01-07 10:38:26 +1300

    pushing hello API to repo

--- Deploy/deploy-api.ps1
+++ Deploy/deploy-api.ps1
@@ -8,6 +8,7 @@
  #Compress the file into a zip -Force: Pushes things forward if there is an existing file with the same name
  Compress-Archive -Path .\bin\Debug\netcoreapp3.1\publish\** -DestinationPath healthCheck-api.zip -Force
  #Push to Azure  
- az webapp deployment source config-zip --resource-group players-rg --name players-aue --src healthCheck-api.zip
+ az webapp deployment source config-zip --resource-group players-rg --name players-aue-api --src healthCheck-api.zip
  #pops the pathway attached to the location stack  
  Pop-Location 
+ start https://players-aue-api.azurewebsites.net/players 

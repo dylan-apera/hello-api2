47a3c8cee093fe7aba949df809b75d75d2c3da85
dylan-apera <74391707+dylan-apera@users.noreply.github.com>
2021-01-07 11:48:08 +1300

    Added new player

--- HealthCheck/Controllers/Players.cs
+++ HealthCheck/Controllers/Players.cs
@@ -18,7 +18,7 @@ namespace HealthCheck.Controllers
             var players = new[] {
                 new { Name = "Dan", Email = "dan@microsoft.com", Address = "123 Street", PhoneNo = "1234", Rank = 1000},
                 new { Name = "Dylan", Email = "dylan@microsoft.com", Address = "126 Street", PhoneNo = "122224", Rank = 1001},
-                new { Name = "Dylan1", Email = "dylan1@microsoft.com", Address = "1216 Street", PhoneNo = "000", Rank = 1031}
+                new { Name = "Dylan1", Email = "dylan1@microsoft.com", Address = "1216 Street", PhoneNo = "000", Rank = 1031},
                 new { Name = "Dan1", Email = "dan1@microsoft.com", Address = "109 Street", PhoneNo = "1200", Rank = 1010},
             };
 

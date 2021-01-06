1c74d0f97615e405aab49b6bd5d83a9cf7dcc626
dylan-apera <74391707+dylan-apera@users.noreply.github.com>
2021-01-07 11:28:45 +1300

    pushing deployment files

--- Deploy/deploy-azure.ps1
+++ Deploy/deploy-azure.ps1
@@ -1,3 +1,4 @@
 az group create -n players-rg -l australiaeast 
 az appservice plan create -g players-rg -n players-aue-plan
 az webapp create -g players-rg -p players-aue-plan -n players-aue-api
+az webapp deployment source config --branch main --name players-aue-api --repo-url https://github.com/dylan-apera/hello-api2.git --resource-group players-rg 

e947ae97587350713c8b2d343fedba9ccaf9432c
Dylan Apera <dylan.n.apera@hotmail.com>
2021-02-04 15:41:43 +1300

    commit changes to deploy-auzre.ps1

--- Deploy/deploy-azure.ps1
+++ Deploy/deploy-azure.ps1
@@ -1,4 +1,9 @@
-az group create -n players-rg -l australiaeast 
-az appservice plan create -g players-rg -n players-aue-plan
-az webapp create -g players-rg -p players-aue-plan -n players-aue-api
-az webapp deployment source config --branch main --name players-aue-api --repo-url https://github.com/dylan-apera/hello-api2.git --resource-group players-rg 
+$rg = 'players-rg'
+$plan = 'players-aue-plan'
+$app = 'players-aue-api'
+$location = 'australiaeast'
+
+az group create -n $rg -l $location 
+az appservice plan create -g $rg -n $plan
+az webapp create -g $rg -p $plan -n $app
+az webapp deployment source config --branch main --name $app --repo-url https://github.com/dylan-apera/hello-api2.git --resource-group $rg 
